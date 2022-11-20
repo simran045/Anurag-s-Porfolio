243fe53badb34a49a1ab24e8693ec80bacd8b66d
AnuragCool <anuragcooldavkh@gmal.com>
2022-11-20 13:29:47 +0530

    updated parallax effect

--- public/js/script.js
+++ public/js/script.js
@@ -8,8 +8,8 @@ document.addEventListener('mousemove', parallax);
 function parallax(e) {
   document.querySelectorAll('.object').forEach(function (move) {
     var moving_value = move.getAttribute('data-value');
-    var x = (e.clientX + moving_value) / 250;
-    var y = (e.clientY + moving_value) / 250;
+    var x = -((e.clientX + moving_value) / 200);
+    var y = -((e.clientY + moving_value) / 200);
 
     move.style.transform = 'translateX(' + x + 'px) translateY(' + y + 'px)';
   });
